a5abb8466d603f0930eed56b897445e2b6db578a
DouglasTSamuel <douglas.netf@gmail.com>
2021-06-01 10:41:40 -0300

    updateNaCalculadora

--- calculadoraImc/script.js
+++ calculadoraImc/script.js
@@ -1,40 +1,59 @@
 let botaoCalcular = document.getElementById("botaoCalcular");
 
-function calculandoImc(){
+function calculandoImc() {
   let peso = document.getElementById("numericPeso").value;
   let rangePeso = document.getElementById("rangePeso").value;
-  let altura = document.getElementById("numericAltura").value/100;
-  let rangeAltura = document.getElementById("rangeAltura").value/100;
- 
- 	if(altura !== "" && peso !== ""){
-		let imc = (peso/(altura * altura)).toFixed(2);
-		let mensagem = "";
+  let altura = document.getElementById("numericAltura").value / 100;
+  let rangeAltura = document.getElementById("rangeAltura").value / 100;
 
-		if(imc < 18.5){
-			mensagem = "Você está abaixo do peso !!!"
-		}
-		else if(imc < 25){
-			mensagem = "Você está no peso ideal !!!"
-		}
-		else if(imc < 30){
-			mensagem = "Você está levemente acima do peso !!!"
-		}
-		else if(imc < 35){
-			mensagem = "Cuidado! Obesidade grau I !!!"
-		}
-		else if(imc < 40){
-			mensagem = "Cuidado! Obesidade grau II !!!"
-		}
-		else {
-			mensagem = "Cuidado! Obesidade grau III !!!"
-		}
+  if (altura !== "" && peso !== "") {
+    let imc = (peso / (altura * altura)).toFixed(2);
+    let imc2 = (rangePeso / (rangeAltura * rangeAltura)).toFixed(2);
+    let mensagem = "";
 
-		resultado.textContent = `Seu IMC é ${imc}.</br>${mensagem}`;
-	}else{
-		resultado.textContent="Preencha Todos os campos !!!"
+    if (imc < 18.5 || imc2 < 18.5) {
+      mensagem =
+        "Você está abaixo do peso !!!  DICA: Coma uma pouco mais, vamos? !!! 🥘";
+      resultado.style.backgroundColor = "white";
+      resultado.style.backgroundColor = "#99eE99";
+      resultado.style.color="black";
+    } else if (imc < 25 || imc2 < 25) {
+      mensagem =
+        "Você está no peso ideal !!! DICA: Se mantenha assim, arrasou!!👏";
+      resultado.style.backgroundColor = "white";
+      resultado.style.color="white";
+      resultado.style.backgroundColor = "green";
+    } else if (imc < 30 || imc2 < 30) {
+      mensagem =
+        "Você está levemente acima do peso !!!  DICA: Comece a se controlar, confio em você !!! 👏";
+      resultado.style.backgroundColor = "white";
+      resultado.style.color="black";
+      resultado.style.backgroundColor = "#99eE99";
+    } else if (imc < 35 || imc2 < 35) {
+      mensagem =
+        "Cuidado! Obesidade grau I !!!  DICA: Vamos começar a se exercicitar ?? vem comigo !!🏋️‍♀️";
+      resultado.style.backgroundColor = "white";
+      resultado.style.color="white";
+      resultado.style.backgroundColor = "orange";
+    } else if (imc < 40 || imc2 < 40) {
+      mensagem =
+        "Cuidado! Obesidade grau II !!!  DICA: Não desista, você consegue EMAGRECER, só basta querer🏋️‍♀️🧠!!";
+      resultado.style.backgroundColor = "white";
+      resultado.style.color="white";
+      resultado.style.backgroundColor = "red";
+    } else {
+      mensagem =
+        "Cuidado! Obesidade grau III !!!  DICA: Procure um médico/nutricionista, cuide da sua saúde !! 🏋️‍♀️🧠💪🥕";
+      resultado.style.backgroundColor = "white";
+      resultado.style.color="white";
+      resultado.style.backgroundColor = "#8b0000";
+    }
+    resultado.textContent = `Seu IMC é ${imc}.`;
+    mensagemImc.textContent = `${mensagem}`;
+  } else {
+    resultado.textContent = "Preencha Todos os campos !!!";
     numericPeso.textContent = "";
-    numericPeso.textContent ="";
-	}
+  }
 }
 
-botaoCalcular.addEventListener("click",calculandoImc);
+botaoCalcular.addEventListener("click", calculandoImc);
